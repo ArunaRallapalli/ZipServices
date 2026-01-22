c06f278d63ca3c1e7002a499be776d5bf959fc70
arallapalli <arunarallapalli12@gmail.com>
2026-01-21 21:50:36 -0700

    Update CORS origins

--- backend/server.ts
+++ backend/server.ts
@@ -55,9 +55,10 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
+  
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Accept', 'Authorization']
- // credentials: true
+  allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  credentials: true
 }));
 
 
